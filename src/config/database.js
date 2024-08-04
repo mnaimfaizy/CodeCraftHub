7269df3170cab39cf9f5c424c54a2b8edfa77322
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-08-03 21:53:14 -0400

    Adding temporary changes to Github

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../models/userModel');
+const User = require('../models/UserModel');
 
 const connectDB = async () => {
   try {
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/userModel');
+const User = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
